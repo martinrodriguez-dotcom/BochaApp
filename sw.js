57a6a2349de46d6abf820fe6abb795729d41754b
martinrodriguez-dotcom <martin.rodriguez@siipallets.com>
2026-02-26 11:42:38 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Dejamos que el navegador maneje las peticiones por defecto
+});
